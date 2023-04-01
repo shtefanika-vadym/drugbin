de8fb3af7c19d18c1682cf266deb866aadf93110
CostescuAdrian <adrian.costescu@assist.ro>
2023-04-01 18:02:51 +0300

    feat(drug-2): Create button component
    refactoring

---
+++ src/common/components/Button/Button.styled.ts
@@ -0,0 +1,96 @@
+import styled, { css } from 'styled-components'
+
+import {
+  WDS_COLOR_BLUE_200,
+  WDS_COLOR_BLUE_300,
+  WDS_COLOR_BLUE_400,
+  WDS_COLOR_WHITE,
+} from 'common/style/colors'
+import { border } from 'common/style/mixins/border.mixin'
+import { flex } from 'common/style/mixins/flex.mixin'
+import { textVariant } from 'common/style/mixins/typography.mixin'
+import {
+  WDS_SIZE_004_PX,
+  WDS_SIZE_006_PX,
+  WDS_SIZE_008_PX,
+  WDS_SIZE_010_PX,
+  WDS_SIZE_024_PX,
+} from 'common/style/size'
+
+import type { StyledButtonProps } from './Button.types'
+
+const fontNormal = css`
+  ${textVariant('subheading')};
+`
+
+export const buttonVariants = {
+  primary: css`
+    color: ${WDS_COLOR_WHITE};
+    background-color: ${WDS_COLOR_BLUE_300};
+    border-radius: ${WDS_SIZE_008_PX};
+    &:hover {
+      background-color: ${WDS_COLOR_BLUE_400};
+    }
+  `,
+  secondary: css`
+    color: ${WDS_COLOR_BLUE_300};
+    background-color: transparent;
+    ${border({ type: 'solid', color: WDS_COLOR_BLUE_300 })}
+    border-radius: ${WDS_SIZE_008_PX};
+    &:hover {
+      background-color: ${WDS_COLOR_BLUE_300};
+      color: ${WDS_COLOR_WHITE};
+    }
+    &:disabled {
+      ${border({ type: 'solid', color: WDS_COLOR_BLUE_300 })}
+    }
+  `,
+  square: css`
+    background-color: ${WDS_COLOR_BLUE_200};
+    border-radius: ${WDS_SIZE_004_PX};
+    &:hover {
+      transition: transform 0.3s ease-out;
+      transform: translateY(-1px);
+    }
+  `,
+  round: css`
+    background-color: ${WDS_COLOR_BLUE_200};
+    border-radius: 50%;
+    &:hover {
+      transition: transform 0.3s ease-out;
+      transform: translateY(-1px);
+    }
+  `,
+}
+
+export const buttonSizes = {
+  S: css`
+    ${fontNormal};
+    padding: ${WDS_SIZE_010_PX} ${WDS_SIZE_024_PX};
+    gap: ${WDS_SIZE_010_PX};
+  `,
+  'S-square': css`
+    padding: ${WDS_SIZE_004_PX};
+  `,
+  'S-round': css`
+    padding: ${WDS_SIZE_006_PX};
+  `,
+}
+
+export const StyledButton = styled.button<StyledButtonProps>`
+  all: unset;
+  ${flex({ direction: 'row', justifyContent: 'center', alignItems: 'center' })}
+  ${({ variant }) => buttonVariants[variant]}
+  ${({ size }) => buttonSizes[size]}
+  transition-property: background-color, border-color;
+  transition-duration: 0.2s;
+  box-sizing: border-box;
+  cursor: pointer;
+  gap: ${WDS_SIZE_008_PX};
+  &:focus {
+    outline: solid 1px ${WDS_COLOR_BLUE_400};
+  }
+  &:disabled {
+    cursor: not-allowed;
+  }
+`
---
+++ src/common/components/Button/Button.types.ts
@@ -0,0 +1,10 @@
+import type { ButtonHTMLAttributes } from 'react'
+
+export type ButtonVariant = 'primary' | 'secondary' | 'square' | 'round'
+export type ButtonSize = 'S' | 'S-square' | 'S-round'
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant
+  size?: ButtonSize
+}
+
+export type StyledButtonProps = Required<Pick<ButtonProps, 'variant' | 'size'>>
