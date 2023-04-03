2981208c2967be9c30d944957ef05d95d2d6b273
CostescuAdrian <adrian.costescu@assist.ro>
2023-04-03 09:33:30 +0300

    feat(drug-5): Create dropdown
    feat(drug-5): Delete some data

---
+++ src/common/components/Dropdown/Dropdown.types.ts
@@ -0,0 +1,7 @@
+export interface DropdownProps {
+  label?: string
+  placeholder: string
+  selectedOptions: string
+  options: Array<string>
+  callbackOnChange?: (selectedOption: string) => void
+}
